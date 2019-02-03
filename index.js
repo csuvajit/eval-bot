e2d2cc19bbc7140ec146ae582b9614a7c8725fc6
unknown <suvajit.c@outlook.in>
2019-02-03 19:59:17 +0530

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on("ready", () => {
   client.user.setActivity(`eval`, {type:"STREAMING"});
 });
 
-client.on("message", async(message) => {
+client.on("message", async (message) => {
 
   if(!message.content.startsWith(prefix)) return;
   if (message.author.bot) return;
@@ -34,7 +34,6 @@ client.on("message", async(message) => {
 
         output = inspect(output, { depth: 0, maxArrayLength: null });
         output = output.replace(filter, "[TOKEN]");
-        output = output.replace(/IzNDEy.DwiOhg.x8VWYL6lwD2H/g, '')
         output = clean(output);
 
       if (output.length < 1950) {
@@ -56,8 +55,22 @@ client.on("message", async(message) => {
 		return message.channel.send(`=== HELP ===\n\n${prefix}eval\n${prefix}help\n${prefix}invite \n\n=== DISCLAIMER ===\nThis bot is an eval which runs any JavaScript code which is like users can even wreck your server mass dm or anything so use it at your own risk I'm not responsible if someone did a malicious code that destroys your server. For safety the invite link does not add any permissions to the bot it's up to you to give it permissions if you want but as I said at your own risk I'm not responsible for bad things happening in your server. KEEP IT NO PERMISSIONS FOR SAFETY, and those hackers reading this don't think of hacking me, the token is safe, and I'm using heroku which doesn't give access to my files and it's free host thing idc much`, {code: "asciidoc"});
 
 	} else if(command === "invite") {
-		return message.channel.send("By inviting me to your server you agree that I'm not responsible for any damage on your server for safety the URL doesn't grant the bot any permissions\nhttps://discordapp.com/api/oauth2/authorize?client_id=" + client.user.id + "&permissions=0&scope=bot")
-	}
+    return message.channel.send("By inviting me to your server you agree that I'm not responsible for any damage on your server for safety the URL doesn't grant the bot any permissions\nhttps://discordapp.com/api/oauth2/authorize?client_id=" + client.user.id + "&permissions=0&scope=bot");
+    
+	} else if (command === 'docs') {
+
+		let project = 'main';
+		let branch = ['stable', 'master', 'rpc', 'commando'].includes(args.slice(-1)[0]) ? args.pop() : 'stable';
+		if (['rpc', 'commando'].includes(branch)) {
+			project = branch;
+			branch = 'master';
+		}
+		const queryString = qs.stringify({ q: args.join(' ') });
+		const res = await fetch(`https://djsdocs.sorta.moe/${project}/${branch}/embed?${queryString}`);
+		const embed = await res.json();
+    if (!embed) return;
+		await message.channel.send({ embed });
+  }
 	
 });
 
