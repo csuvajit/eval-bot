82d9dd626235696dbad59e4d6dea8a1ef00f6894
unknown <suvajit.c@outlook.in>
2019-02-06 20:03:40 +0530

    no changes

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ client.on("message", async (message) => {
 
   if(!message.content.startsWith(prefix)) return;
   if (message.author.bot) return;
+  
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
