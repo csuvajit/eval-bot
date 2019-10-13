357881baacdc8d7245d3b3352bd269f6d9e919ec
Suvajit <suvajit.me@gmail.com>
2019-10-13 20:30:05 +0530

    embed invite url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const { inspect } = require('util');
 
 client.on('ready', () => {
-	console.log(`LOGGED IN AS : ${client.user.tag}`);
+	console.log(`[READY] ${client.user.tag}`);
 	client.user.setActivity('eval', { type: 'STREAMING' });
 });
 
@@ -14,7 +14,7 @@ function clean(text) {
 }
 
 client.on('message', async message => {
-	const prefix = process.env.DISCORD_PREFIX;
+	const prefix = process.env.PREFIX;
 	if (!message.content.startsWith(prefix)) return;
 	if (message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -32,7 +32,7 @@ client.on('message', async message => {
 			if (output instanceof Promise || (Boolean(output) && typeof output.then === 'function' && typeof output.catch === 'function')) output = await output;
 
 			output = inspect(output, { depth: 0, maxArrayLength: null });
-			output = output.replace(filter, '[TOKEN]');
+			output = output.replace(filter, '--🙄--');
 			output = clean(output);
 
 			if (output.length < 1950) {
@@ -43,11 +43,22 @@ client.on('message', async message => {
 			return message.channel.send(`The following error occured \`\`\`js\n${error}\`\`\``);
 		}
 	} else if (command === 'invite') {
-		return message.channel.send([
-			'By inviting me to your server you agree that I\'m not responsible for any damage on your server!',
-			'For safety the URL doesn\'t grant the bot any permissions!',
-			`<https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot>`
-		]);
+		const qs = require('querystring');
+		const query = qs.stringify({
+			client_id: client.user.id,
+			permissions: 0,
+			scope: 'bot'
+		});
+		const embed = new Discord.MessageEmbed()
+			.setAuthor(client.user.username, client.user.displayAvatarURL())
+			.setColor(0xf30c11)
+			.setDescription([
+				'By inviting me to your guild you agree that I\'m not responsible for any damage on your guild.',
+				'For safety the URL doesn\'t grant the bot any permissions.',
+				'',
+				`[Invite Me](https://discordapp.com/api/oauth2/authorize?${query})`
+			]);
+		return message.channel.send({ embed });
 	}
 });
 
